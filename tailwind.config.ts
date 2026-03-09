480902151a7a3358ebb567957f3b9c91df1a9da7
Daniyal-lone <62689207+Daniyal-lone@users.noreply.github.com>
2026-03-09 17:34:56 +0530

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  prefix: "",
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      }
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
